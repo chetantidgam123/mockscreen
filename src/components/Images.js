f39fca0b8cc630399da26757e95a12556b9c8f7b
chetan <chetan@shipyaari.com>
2023-04-15 10:50:54 +0530

    final

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,7 +1,7 @@
 import { Box, Image } from '@chakra-ui/react';
 import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux';
-
+import '../CSS/Navitems.css'
 const Images = () => {
     const state = useSelector((state) => state.last)
     useEffect(() => {
@@ -11,10 +11,11 @@ const Images = () => {
         <div style={{ width: "100%", display: "flex", justifyContent: "space-around" }}>
             {
                 state?.map((ele, i) => (
-                    <Box mx={1} key={i} >
+                    <Box mx={1} key={i} position={'reletive'}>
                         {
                             <Image src={ele.image} width={'300px'} height={'450px'} />
                         }
+                        <Box className='shadow'>{ele.name}</Box>
 
                     </Box>
                 ))
